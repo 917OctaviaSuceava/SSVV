d0ed24f6947eb34669bb8a0689e30c72da38fc04
AlexandruStinga <alexstinga702@gmail.com>
2023-03-29 21:54:55 +0300

    downgraded tests

--- src/test/java/service/ServiceTest.java
+++ src/test/java/service/ServiceTest.java
@@ -1,9 +1,7 @@
 package service;
 
 import domain.Student;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import repository.NotaXMLRepository;
 import repository.StudentXMLRepository;
 import repository.TemaXMLRepository;
@@ -11,12 +9,11 @@ import validation.NotaValidator;
 import validation.StudentValidator;
 import validation.TemaValidator;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
-class ServiceTest {
+public class ServiceTest {
     Service service;
-    @BeforeEach
-    void setUp() {
+    public void setUp() {
         StudentValidator studentValidator = new StudentValidator();
         TemaValidator temaValidator = new TemaValidator();
         NotaValidator notaValidator = new NotaValidator();
@@ -29,18 +26,16 @@ class ServiceTest {
         this.service = new Service(studentXMLRepository, temaXMLRepository, notaXMLRepository);
     }
 
-    @AfterEach
-    void tearDown() {
-    }
-
     @Test
-    void testSaveStudent() {
+    public void testSaveStudent() {
+        this.setUp();
         Student student = new Student("1", "Ion", 931);
         int result = this.service.saveStudent(student.getID(), student.getNume(), student.getGrupa());
         assertEquals(0, result);
     }
     @Test
-    void testSaveStudentNullId() {
+    public void testSaveStudentNullId() {
+        this.setUp();
         Student student = new Student("", "Ion", 931);
         int result = this.service.saveStudent(student.getID(), student.getNume(), student.getGrupa());
         assertEquals(1, result);
@@ -48,7 +43,8 @@ class ServiceTest {
 
     @Test
     //test for null name
-    void testSaveStudentNullName() {
+    public void testSaveStudentNullName() {
+        this.setUp();
         Student student = new Student("1", "", 931);
         int result = this.service.saveStudent(student.getID(), student.getNume(), student.getGrupa());
         assertEquals(1, result);
@@ -56,7 +52,8 @@ class ServiceTest {
 
     @Test
     //test for null group
-    void testSaveStudentNullGroup() {
+    public void testSaveStudentNullGroup() {
+        this.setUp();
         Student student = new Student("1", "Ion", 0);
         int result = this.service.saveStudent(student.getID(), student.getNume(), student.getGrupa());
         assertEquals(1, result);
@@ -64,14 +61,16 @@ class ServiceTest {
 
     @Test
     //test for -1 group
-    void testSaveStudentMinusOneGroup() {
+    public void testSaveStudentMinusOneGroup() {
+        this.setUp();
         Student student = new Student("1", "Ion", -1);
         int result = this.service.saveStudent(student.getID(), student.getNume(), student.getGrupa());
         assertEquals(1, result);
     }
 
     @Test
-    void testSavingNullStudent() {
+    public void testSavingNullStudent() {
+        this.setUp();
         Student student = new Student(null, null, 0);
         int result = this.service.saveStudent(student.getID(), student.getNume(), student.getGrupa());
         assertEquals(1, result);
